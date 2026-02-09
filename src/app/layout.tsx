7202b5b34d24e1081dc0c4f411bc9dbc7576e7d1
Daniel <admin@interact.com.uy>
2026-02-09 15:24:17 -0300

    logo nuestro trabajo que no se veia e instalacion de analytics de vercel

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Work_Sans, DM_Sans } from "next/font/google";
 import "./globals.css";
+import { SpeedInsights } from "@vercel/speed-insights/next"
 
 import Header from "@/components/layout/Header";
 import Footer from "@/components/layout/Footer";
