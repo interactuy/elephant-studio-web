084dd70de9517664a8e29fb3712aa0c45ca7dc90
Daniel <admin@interact.com.uy>
2026-03-10 12:16:06 -0300

    borrado de listener ga + agregado de GTM

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,4 @@
 // src/app/layout.tsx
-import { Suspense } from "react";
 import type { Metadata } from "next";
 import { Work_Sans, DM_Sans } from "next/font/google";
 import Script from "next/script";
@@ -9,11 +8,10 @@ import Header from "@/components/layout/Header";
 import Footer from "@/components/layout/Footer";
 
 import { SpeedInsights } from "@vercel/speed-insights/next";
-import AnalyticsListener from "@/components/AnalyticsListener";
 
 const workSans = Work_Sans({
   subsets: ["latin"],
-  weight: ["600"], // SemiBold
+  weight: ["600"],
   variable: "--font-work",
   display: "swap",
 });
@@ -50,40 +48,34 @@ export const metadata: Metadata = {
   },
 };
 
+const GTM_ID = "GTM-PJZN4QNF";
+
 export default function RootLayout({
   children,
 }: Readonly<{ children: React.ReactNode }>) {
-  const GA_ID = process.env.NEXT_PUBLIC_GA_ID;
-
   return (
     <html lang="es" className={`${workSans.variable} ${dmSans.variable}`}>
       <head>
-        {GA_ID && (
-          <>
-            <Script
-              src={`https://www.googletagmanager.com/gtag/js?id=${GA_ID}`}
-              strategy="afterInteractive"
-            />
-            <Script id="ga-init" strategy="afterInteractive">
-              {`
-                window.dataLayer = window.dataLayer || [];
-                function gtag(){dataLayer.push(arguments);}
-                gtag('js', new Date());
-                gtag('config', '${GA_ID}', {
-                  page_path: window.location.pathname,
-                });
-              `}
-            </Script>
-          </>
-        )}
+        <Script id="gtm-script" strategy="afterInteractive">
+          {`
+            (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+            new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+            'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+            })(window,document,'script','dataLayer','${GTM_ID}');
+          `}
+        </Script>
       </head>
 
       <body>
-        {/* GA pageviews para navegación (App Router) */}
-      <Suspense fallback={null}>
-        <AnalyticsListener />
-      </Suspense>
-
+        <noscript>
+          <iframe
+            src={`https://www.googletagmanager.com/ns.html?id=${GTM_ID}`}
+            height="0"
+            width="0"
+            style={{ display: "none", visibility: "hidden" }}
+          />
+        </noscript>
 
         <Header />
         {children}
